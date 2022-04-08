3895ab47dedbe4e073417e51f5bdc164b049aa93
jobarv <jbarrera@vabaja.com.mx>
2022-04-07 20:53:26 -0500

    se agrega boton cambio de color

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
+//Se pretende realizar un carrito de compras para la tienda de Barras Designs
+//El usuario podrá agregar, borrar y visualizar el carrito de compras.
+// Se deberá guardar la sesión del usuario con nombre de usuario y preparar todo para eventualmente integrar una base de datos.
+
+//función cambio de color de Fondo
+function cambiarModo() {
+    let cuerpoweb = document.body;
+    cuerpoweb.classList.toggle("oscuro");
+  }
+// Variables
 let Playera;
+// let nombre;
 
 function agregarPlayera() {
     alert("Playera Agregada al Carrito");
 }
+
+//Sesión del Usuario
+
+// "Agregar Carrito"
+
+// Elimina playera del carrito
+
+
+
+// Elimina la playera del carrito en el DOM
+
+
+
+// Muestra la playera seleccionada en el Carrito
+
+
+// Elimina las playeras del carrito en el DOM
+
+
