abea67c329c640769d2c9e31196b111e525adf02
jobarv <jbarrera@vabaja.com.mx>
2022-09-20 12:10:33 -0500

    primer version menu hamburguesa

--- js/main.js
+++ js/main.js
@@ -1,36 +1,8 @@
-/* const btnMenu = document.querySelector("#btnMenu");
-const menu = document.querySelector("#menu");
-
-btnMenu.addEventListener("click", function () {
-    menu.classList.toggle("mostrar");
-    menu.classList.("");
-});
-
-const subMenuBtn = document.querySelector(".submenu-btn");
-for (let i = 0; i < subMenuBtn.clientHeight; i++) {
-    subMenuBtn[i].addEventListener("click", function () {
-        if (window.innerWidth < 1024) {
-            const subMenu = this.nextElementSibling;
-            const height = subMenu.scrollHeight;
-
-            if (subMenu.classList.contains("desplegar")) {
-                subMenu.classList.remove("desplegar");
-                subMenu.removeAttribute("style");
-
-            } else {
-
-                subMenu.classList.add("desplegar");
-                subMenu.style.height = height + "px"
-            }
-        }
-    });
-} */
-
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "block";
+        x.style.display = "block";
     }
-  }
+}
